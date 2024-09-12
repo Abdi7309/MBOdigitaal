5c85d8a01398d58a0bb954cc5a09daab648c64e0
Abdi7309 <512875@vistacollege.nl>
2024-09-12 09:57:19 +0200

    1

--- views/keuzedelen/challenges.php
+++ views/keuzedelen/challenges.php
@@ -14,6 +14,32 @@
         <p class="my-4 font-bold text-gray-700">Hieronder staat het overzicht van die te maken hebben met het
             ontwikkelen van de keuzedelen binnen het systeem van mbogodigital.nl</p>
 
+            <div class="w-full">
+            <div class="flex border-b border-gray-300">
+                <button
+                    class="w-1/2 py-4 text-center font-medium text-gray-700 bg-gray-100 rounded-tl-lg focus:outline-none active:bg-gray-200">
+                    <a href="https://eduofficenl-my.sharepoint.com/personal/m_dackus_vistacollege_nl/_layouts/15/onedrive.aspx?ga=1&id=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen%2FBasis%20programmeren%20van%20games%2Epdf&parent=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen%5C">Basis programmeren (Games)</a></button>
+                <button
+                    class="w-1/2 py-4 text-center font-medium text-gray-700 bg-gray-100 rounded-tl-lg focus:outline-none active:bg-gray-200">
+                    <a href="https://eduofficenl-my.sharepoint.com/personal/m_dackus_vistacollege_nl/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen%2FDigitale%20vaardigheden%20gevorderd%2Epdf&parent=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen">Digitale vaardigheden</a></button>
+                <button
+                    class="w-1/2 py-4 text-center font-medium text-gray-700 bg-gray-100 rounded-tr-lg focus:outline-none active:bg-gray-200">
+                    <a href="https://eduofficenl-my.sharepoint.com/personal/m_dackus_vistacollege_nl/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen%2FFotografie%20basis%2Epdf&parent=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen">Fotografie basis</a></button>
+                <button
+                    class="w-1/2 py-4 text-center font-medium text-gray-700 bg-gray-100 rounded-tr-lg focus:outline-none active:bg-gray-200">
+                    <a href="https://eduofficenl-my.sharepoint.com/personal/m_dackus_vistacollege_nl/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen%2FInspelen%20op%20innovaties%20geschikt%20voor%20niveau%204%2Epdf&parent=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen">Inspelen op inovaties</a></button>
+                <button
+                    class="w-1/2 py-4 text-center font-medium text-gray-700 bg-gray-100 rounded-tr-lg focus:outline-none active:bg-gray-200">
+                    <a href="https://eduofficenl-my.sharepoint.com/personal/m_dackus_vistacollege_nl/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen%2FOndernemend%20gedrag%20%28geschikt%20voor%20niveau%203%20en%204%29%2Epdf&parent=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen">Ondernemend gedrag</a></button>
+                <button
+                    class="w-1/2 py-4 text-center font-medium text-gray-700 bg-gray-100 rounded-tr-lg focus:outline-none active:bg-gray-200">
+                    <a href="https://eduofficenl-my.sharepoint.com/personal/m_dackus_vistacollege_nl/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen%2FOri%C3%ABntatie%20op%20ondernemerschap%2Epdf&parent=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen">Orientatie op ondernemendshap</a></button>
+                <button
+                    class="w-1/2 py-4 text-center font-medium text-gray-700 bg-gray-100 rounded-tr-lg focus:outline-none active:bg-gray-200">
+                    <a href="https://eduofficenl-my.sharepoint.com/personal/m_dackus_vistacollege_nl/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen%2FVerdieping%20software%2Epdf&parent=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen">Verdieping software</a></button>
+            </div>
+
+
         <div class="w-full">
             <div class="flex border-b border-gray-300">
                 <button
