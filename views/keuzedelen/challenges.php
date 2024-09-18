fe37a6f95442167dccd15886c01035e15a1f1e49
Abdi7309 <512875@vistacollege.nl>
2024-09-18 10:39:54 +0200

    Update challenges.php

--- views/keuzedelen/challenges.php
+++ views/keuzedelen/challenges.php
@@ -8,84 +8,40 @@
 <body>
     <?php require '../views/templates/menu.php' ?>
 
-    <div class="mt-6 mb-16 w-11/12 p-6 space-y-8 sm:p-8 bg-white mx-auto">
+    <div class="w-11/12 p-6 space-y-8 sm:p-8 bg-white mx-auto">
 
         <h2 class="text-2xl font-bold dark:text-white">Keuzedelen</h2>
-        <p class="my-4 font-bold text-gray-700">Hieronder staat het overzicht van die te maken hebben met het
-            ontwikkelen van de keuzedelen binnen het systeem van mbogodigital.nl</p>
-
             <div class="w-full">
-            <div class="flex border-b border-gray-300">
+            <div class="flex flex-col">
                 <button
-                    class="w-1/2 py-4 text-center font-medium text-gray-700 bg-gray-100 rounded-tl-lg focus:outline-none active:bg-gray-200">
-                    <a href="https://eduofficenl-my.sharepoint.com/personal/m_dackus_vistacollege_nl/_layouts/15/onedrive.aspx?ga=1&id=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen%2FBasis%20programmeren%20van%20games%2Epdf&parent=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen%5C">Basis programmeren (Games)</a></button>
+                    class="w-full py-4 text-center font-medium text-gray-700 bg-gray-100 focus:outline-none active:bg-gray-200 rounded-tl-lg rounded-tr-lg">
+                    <a href="https://eduofficenl-my.sharepoint.com/personal/m_dackus_vistacollege_nl/_layouts/15/onedrive.aspx?ga=1&id=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen%2FBasis%20programmeren%20van%20games%2Epdf&parent=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen">Basis programmeren (Games)</a>
+                </button>
                 <button
-                    class="w-1/2 py-4 text-center font-medium text-gray-700 bg-gray-100 rounded-tl-lg focus:outline-none active:bg-gray-200">
-                    <a href="https://eduofficenl-my.sharepoint.com/personal/m_dackus_vistacollege_nl/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen%2FDigitale%20vaardigheden%20gevorderd%2Epdf&parent=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen">Digitale vaardigheden</a></button>
+                    class="w-full py-4 my-2 text-center font-medium text-gray-700 bg-gray-100 focus:outline-none active:bg-gray-200">
+                    <a href="https://eduofficenl-my.sharepoint.com/personal/m_dackus_vistacollege_nl/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen%2FDigitale%20vaardigheden%20gevorderd%2Epdf&parent=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen">Digitale vaardigheden</a>
+                </button>
                 <button
-                    class="w-1/2 py-4 text-center font-medium text-gray-700 bg-gray-100 rounded-tr-lg focus:outline-none active:bg-gray-200">
-                    <a href="https://eduofficenl-my.sharepoint.com/personal/m_dackus_vistacollege_nl/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen%2FFotografie%20basis%2Epdf&parent=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen">Fotografie basis</a></button>
+                    class="w-full py-4 text-center font-medium text-gray-700 bg-gray-100 focus:outline-none active:bg-gray-200">
+                    <a href="https://eduofficenl-my.sharepoint.com/personal/m_dackus_vistacollege_nl/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen%2FFotografie%20basis%2Epdf&parent=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen">Fotografie basis</a>
+                </button>
                 <button
-                    class="w-1/2 py-4 text-center font-medium text-gray-700 bg-gray-100 rounded-tr-lg focus:outline-none active:bg-gray-200">
-                    <a href="https://eduofficenl-my.sharepoint.com/personal/m_dackus_vistacollege_nl/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen%2FInspelen%20op%20innovaties%20geschikt%20voor%20niveau%204%2Epdf&parent=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen">Inspelen op inovaties</a></button>
+                    class="w-full py-4 my-2 text-center font-medium text-gray-700 bg-gray-100 focus:outline-none active:bg-gray-200">
+                    <a href="https://eduofficenl-my.sharepoint.com/personal/m_dackus_vistacollege_nl/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen%2FInspelen%20op%20innovaties%20geschikt%20voor%20niveau%204%2Epdf&parent=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen">Inspelen op innovaties</a>
+                </button>
                 <button
-                    class="w-1/2 py-4 text-center font-medium text-gray-700 bg-gray-100 rounded-tr-lg focus:outline-none active:bg-gray-200">
-                    <a href="https://eduofficenl-my.sharepoint.com/personal/m_dackus_vistacollege_nl/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen%2FOndernemend%20gedrag%20%28geschikt%20voor%20niveau%203%20en%204%29%2Epdf&parent=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen">Ondernemend gedrag</a></button>
+                    class="w-full py-4 text-center font-medium text-gray-700 bg-gray-100 focus:outline-none active:bg-gray-200">
+                    <a href="https://eduofficenl-my.sharepoint.com/personal/m_dackus_vistacollege_nl/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen%2FOndernemend%20gedrag%20%28geschikt%20voor%20niveau%203%20en%204%29%2Epdf&parent=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen">Ondernemend gedrag</a>
+                </button>
                 <button
-                    class="w-1/2 py-4 text-center font-medium text-gray-700 bg-gray-100 rounded-tr-lg focus:outline-none active:bg-gray-200">
-                    <a href="https://eduofficenl-my.sharepoint.com/personal/m_dackus_vistacollege_nl/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen%2FOri%C3%ABntatie%20op%20ondernemerschap%2Epdf&parent=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen">Orientatie op ondernemendshap</a></button>
+                    class="w-full py-4 my-2 text-center font-medium text-gray-700 bg-gray-100 focus:outline-none active:bg-gray-200">
+                    <a href="https://eduofficenl-my.sharepoint.com/personal/m_dackus_vistacollege_nl/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen%2FOri%C3%ABntatie%20op%20ondernemerschap%2Epdf&parent=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen">Oriëntatie op ondernemendshap</a>
+                </button>
                 <button
-                    class="w-1/2 py-4 text-center font-medium text-gray-700 bg-gray-100 rounded-tr-lg focus:outline-none active:bg-gray-200">
-                    <a href="https://eduofficenl-my.sharepoint.com/personal/m_dackus_vistacollege_nl/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen%2FVerdieping%20software%2Epdf&parent=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen">Verdieping software</a></button>
+                    class="w-full py-4 ext-center font-medium text-gray-700 bg-gray-100 focus:outline-none active:bg-gray-200 rounded-bl-lg rounded-br-lg">
+                    <a href="https://eduofficenl-my.sharepoint.com/personal/m_dackus_vistacollege_nl/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen%2FVerdieping%20software%2Epdf&parent=%2Fpersonal%2Fm%5Fdackus%5Fvistacollege%5Fnl%2FDocuments%2FCBL%2Fmbogodigital%2Enl%2FPeriode%209%2FChallenge%2025%2FKeuzedelen">Verdieping software</a>
+                </button>
             </div>
-
-
-        <div class="w-full">
-            <div class="flex border-b border-gray-300">
-                <button
-                    class="w-1/2 py-4 text-center font-medium text-gray-700 bg-gray-100 rounded-tl-lg focus:outline-none active:bg-gray-200"
-                    onclick="openTab(event, 'tab1')">Case</button>
-                <button
-                    class="w-1/2 py-4 text-center font-medium text-gray-700 bg-gray-100 rounded-tl-lg focus:outline-none active:bg-gray-200"
-                    onclick="openTab(event, 'tab2')">Challenge 25</button>
-                <button
-                    class="w-1/2 py-4 text-center font-medium text-gray-700 bg-gray-100 rounded-tr-lg focus:outline-none active:bg-gray-200"
-                    onclick="openTab(event, 'tab3')">Challenge 26</button>
-                <button
-                    class="w-1/2 py-4 text-center font-medium text-gray-700 bg-gray-100 rounded-tr-lg focus:outline-none active:bg-gray-200"
-                    onclick="openTab(event, 'tab4')">Challenge 27</button>
-            </div>
-            <div id="tab1" class="tabcontent p-4">
-                <?php require 'challenges-tab1.inc.php' ?>
-            </div>
-            <div id="tab2" class="tabcontent p-4 hidden">
-                <?php require 'challenges-tab2.inc.php' ?>
-            </div>
-            <div id="tab3" class="tabcontent p-4 hidden">
-                <?php require 'challenges-tab3.inc.php' ?>
-            </div>
-            <div id="tab4" class="tabcontent p-4 hidden">
-                <?php require 'challenges-tab4.inc.php' ?>
-            </div>
-        </div>
-
-        <script>
-            function openTab(evt, tabName) {
-                var i, tabcontent, tablinks;
-                tabcontent = document.getElementsByClassName("tabcontent");
-                for (i = 0; i < tabcontent.length; i++) {
-                    tabcontent[i].classList.add("hidden");
-                }
-                tablinks = document.getElementsByTagName("button");
-                for (i = 0; i < tablinks.length; i++) {
-                    tablinks[i].classList.remove("active:bg-gray-200");
-                }
-                document.getElementById(tabName).classList.remove("hidden");
-                evt.currentTarget.classList.add("active:bg-gray-200");
-            }
-        </script>
-
-
 
         <?php require '../views/templates/footer.php' ?>
 
